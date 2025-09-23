30c739bb5eb90e6ef020fc369be05127adafc19e
ladyylesca <e42241245@polije.student.ac.id>
2025-09-23 15:03:41 +0700

    Latihan PHP2

---
+++ operatorstring.php
@@ -0,0 +1,12 @@
+<?php
+$a = "Hello ";
+$b = "World!";
+$a = $a . $b;
+echo $a;
+echo "<br />";
+
+$a = "belajar ";
+$b = "PHP ";
+$c = "di Politeknik Negeri Jember";
+$hasil = "Saya sedang " . $a . $b . $c;
+echo $hasil;
